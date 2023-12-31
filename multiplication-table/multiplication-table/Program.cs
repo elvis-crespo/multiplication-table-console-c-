b05814f8b0529ae31942b7dad9a659b309aa53fe
elvis-crespo <eacresposu@gmail.com>
2023-12-31 18:24:38 -0500

    and with that the 2023 season comes to an end

---
+++ multiplication-table/multiplication-table/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine("\tMultiplication table");
+Console.Write("Enter a number: ");
+string? num = Console.ReadLine();
+Multiplication(num);
+
+static void Multiplication(string input)
+{
+    int counter = 1;
+    for (int i = 1; i < 13; i++)
+    {
+        int value = counter++;
+        Console.Write($"{value} * {input} = ");
+
+        for (int j = 0; j < value; j++)
+        {
+            Console.Write($"{input}");
+        }
+        Console.WriteLine();
+    }
+
+    //int num = 12;
+    //for (int i = 1; i <= num; i++)
+    //{
+    //    Console.WriteLine($"{num} * {i} = {num * i}");
+    //}
+}
